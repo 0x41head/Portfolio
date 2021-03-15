0d701e91300591d48ebcbc75632944a156ba633c
Aryan <aryangiroud@Gmail.com>
2021-03-15 12:21:28 +0530

    Navbar added

---
+++ src/Components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <>
+        <div className="nbar">
+            <button className="btn">Menu</button>
+            <button className="btn2">Your Info</button>
+        </div>
+        </>
+        
+    )
+}
+
+export default Navbar
